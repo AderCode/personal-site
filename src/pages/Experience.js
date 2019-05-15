ff816785e1bcd68a6482bb6cc30de0e0ea293099
Matthew Aderhold <aderhold@Matthews-MBP.mshome.net>
2019-05-14 20:05:30 -0500

    changed flavor text for inprogress page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -4,7 +4,7 @@ import { Container, Row, Col } from 'reactstrap';
 const Experience = props => {
     return (
         <Container>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis ipsa nesciunt quod? Temporibus odit tempore commodi quam laborum magni, minus impedit vel repellendus quaerat repellat consequatur officia culpa fuga blanditiis.
+            In progress...
         </Container>
     )
 }
